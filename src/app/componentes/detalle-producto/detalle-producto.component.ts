7807fa03d4b101c3a19d3eaa38a3d0535ed3bcea
Mario Martínez L <mmartinezl@ucenfotec.ac.cr>
2022-07-05 15:19:17 -0600

    se completa funcionalidadpara el componente de detalle de producto

--- src/app/componentes/detalle-producto/detalle-producto.component.ts
+++ src/app/componentes/detalle-producto/detalle-producto.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Producto } from 'src/app/modelos/producto/producto.model';
+import { ProductosService } from 'src/app/servicios/productos.service';
 
 @Component({
   selector: 'app-detalle-producto',
@@ -6,10 +9,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detalle-producto.component.css']
 })
 export class DetalleProductoComponent implements OnInit {
+  productoActual: Producto = new Producto();
+  mensaje = '';
 
-  constructor() { }
+  constructor(
+    private productosService: ProductosService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.mensaje = '';
+    this.obtenerProducto(this.route.snapshot.paramMap.get('id'));
   }
 
+  obtenerProducto(id: any): void {
+    this.productosService.get(id)
+      .subscribe(
+        data => {
+          this.productoActual = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        }
+      );
+  }
+  actualizarProducto(): void {
+    this.productosService.update(this.productoActual.id, this.productoActual)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.mensaje = 'El producto ha sido actualizado satisfactoriamente';
+        },
+        error => {
+          console.log(error);
+        }
+      );
+  }
+  borrarProducto(): void {
+    this.productosService.delete(this.productoActual.id)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.router.navigate(['/productos']);
+        },
+        error => {
+          console.log(error);
+        }
+      );
+  }
 }
